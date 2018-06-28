ad142c89caec7bd168195f8642ad25d81b499aec
Mary Bork <maryebork@outlook.com>
2018-06-28 07:18:30 -0600

    Class Feature/ Credit Check / Pseudo

---
+++ credcheck_class.rb
@@ -0,0 +1,19 @@
+class CreditCheck #is credit check the 'CEO' method? should this be CreditNum
+ def initialize(credNum, luhnNum, doubles, singles, result, mod)
+	@CreditNum = credNum
+	#Not Input? @LuhnNum = luhnNum
+	#Not Input? @Doubles = doubles
+	#Not Input? @Singles = singles
+	#Not Input? @Result = result
+	#Not Input? @Mod = mod
+
+	end
+end
+#DefMeth: CreditCheck #CEO METHOD
+	#DefMeth: s_to_reverse_i_array #CreditNum - LuhnNum
+	#DefMeth: second_element_times_two #LuhnNum - DoublesArray
+	#DefMeth: greater_than_ten_singled #DoublesArray - SinglesArray
+	#DefMeth: add_array # SinglesArray- ResultNum
+	#DefMeth: mod_result #ResultNum - ModNum
+	#DefMeth: number_validation_print #Print ModNum == 0
+
